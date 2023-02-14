b66efccc5ee1393ce26a9886beb9e0d18f5bd3cb
Alexandr Ganitev <122431529+AlexandrGanitev@users.noreply.github.com>
2023-02-14 20:17:58 +0300

    Add files via upload

--- module_3_and_4/check_if_number_palindrom.py
+++ module_3_and_4/check_if_number_palindrom.py
@@ -6,4 +6,18 @@
 
 num = input("Введите восьмизначное число --> ")
 
-print(list(num))
+list_of_strings = num.split() # список строковых представлений чисел
+# print(list_of_strings)
+list_of_numbers = list(map(int, list_of_strings)) # список чисел
+print(list_of_numbers)
+
+if len(list_of_numbers) < 8 or len(list_of_numbers)> 8:
+    print("Вы ввели не 8-значное число")
+if num == num[::-1]:
+    print(f"Это число {num} - палиндром")
+else:
+    print(f"Число {num} не палиндром")
+
+print('*' * 25, "Вариант 2")
+num2 = list_of_numbers
+print((num2) == (num2)[::-1])
