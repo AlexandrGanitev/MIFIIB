84745002450db02a0fca08d561467dc56a3ced9f
Alexandr Ganitev <122431529+AlexandrGanitev@users.noreply.github.com>
2023-02-16 20:29:22 +0300

    Add files via upload

--- module_5/_gamid_scan_ARP_v1.py
+++ module_5/_gamid_scan_ARP_v1.py
@@ -7,8 +7,36 @@ parser.add_argument('-i', '--ip', dest='ip_arg', help='Type ip address here')
 args = parser.parse_args()
 ip = args.ip_arg
 
-arp_req = scapy.ARP()
+arp_req = scapy.ARP(pdst=ip)
+broadcast = scapy.Ether(dst="ff:ff:ff:ff:ff:ff")
+packet = broadcast/arp_req
+# this part doesn't work (sending the packages)
+good, bad = scapy.srp(packet, timeout=1)
+print(good.summary())
+
+#  module_5 % sudo python3 _gamid_scan_ARP_v1.py -i 192.168.1.0/24
+# WARNING: No IPv4 address found on en5 !
+# WARNING: No IPv4 address found on ap1 !
+# WARNING: more No IPv4 address found on awdl0 !
+# Begin emission:
+# Finished sending 256 packets.
+# ..*.*.................................................................................................................................................................................................................
+# Received 214 packets, got 2 answers, remaining 254 packets
+# Ether / ARP who has 192.168.1.1 says 192.168.1.179 ==> Ether / ARP is at 0c:ef:af:d2:5a:82 says 192.168.1.1
+# Ether / ARP who has 192.168.1.2 says 192.168.1.179 ==> Ether / ARP is at f8:f0:82:ae:a8:88 says 192.168.1.2 / Padding
+# None
 
 
-scapy.arping(ip)
+# print('*' * 25, "Output: ")
+# this part works well
+# print("Packet summary: ", packet.summary())
+# print("Packet show: ", packet.show())
+# scapy.arping(ip)
 
+# module_5 % sudo python3 _gamid_scan_ARP_v1.py -i 192.168.1.1/24
+# WARNING: No IPv4 address found on en5 !
+# WARNING: No IPv4 address found on ap1 !
+# WARNING: more No IPv4 address found on awdl0 !
+# WARNING: More than one possible route for Net("192.168.1.1/24")
+# WARNING: More than one possible route for Net("192.168.1.1/24")
+# Ether / ARP who has Net("192.168.1.1/24") says 192.168.1.179
