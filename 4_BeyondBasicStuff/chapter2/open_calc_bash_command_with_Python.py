59012350653b1bb8443c64e6d1dbcf12a7731125
AlexandrGanitev <122431529+AlexandrGanitev@users.noreply.github.com>
2024-03-26 15:03:50 +0300

    Learning Python's path commands.

---
+++ 4_BeyondBasicStuff/chapter2/open_calc_bash_command_with_Python.py
@@ -0,0 +1,10 @@
+import subprocess
+print("When we use the subprocess module in Python to run a bash command, Python sends the command to the system shell, ")
+print("which then executes the command and returns the output back to Python. This process allows us to use the power ")
+print("of bash commands within our Python scripts. Real-World Applications of Running Bash Commands in Python. ")
+print("Running bash commands in Python is not just an academic exercise. It has practical applications in a variety")
+print("of domains, from automating repetitive tasks to managing system processes.")
+result = subprocess.run(['open', '-a', 'calculator'])
+print("\nHere's how to open calculator on Mac:", result)
+
+print("Using zsh command will be just a command: open -a calculator")
---
+++ 4_BeyondBasicStuff/chapter2/posixpath.py
@@ -0,0 +1,9 @@
+# checking absolute Path
+from pathlib import Path
+import os
+
+print("Here's my Home Directory path:", Path.home())
+print("")
+print("Printing my current working directory:", Path.cwd())
+
+
